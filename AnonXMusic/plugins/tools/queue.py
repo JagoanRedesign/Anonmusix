04074b658eff229abceba10a611ffe1430c961ea
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2024-04-07 14:37:07 +0700

    Update queue.py

--- AnonXMusic/plugins/tools/queue.py
+++ AnonXMusic/plugins/tools/queue.py
@@ -36,7 +36,7 @@ def get_duration(playing):
 
 
 @app.on_message(
-    filters.command(["queue", "cqueue", "player", "cplayer", "playing", "cplaying"])
+    filters.command(["queue", "cqueue", "player", "cplayer", "playing","playlist", "cplaying"])
     & filters.group
     & ~BANNED_USERS
 )
@@ -156,7 +156,7 @@ async def queued_tracks(client, CallbackQuery: CallbackQuery, _):
     basic[videoid] = False
     buttons = queue_back_markup(_, what)
     med = InputMediaPhoto(
-        media="https://telegra.ph//file/6f7d35131f69951c74ee5.jpg",
+        media="https://telegra.ph/file/ec5951c55bfde86689abd.jpg",
         caption=_["queue_1"],
     )
     await CallbackQuery.edit_message_media(media=med)
